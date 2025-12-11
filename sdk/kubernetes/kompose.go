00064589792cb985b62dbaf0f4055fa450267fe5
mcbloch <git@mcbloch.dev>
2025-12-11 16:39:41 +0100

    reduce logs

--- sdk/kubernetes/kompose.go
+++ sdk/kubernetes/kompose.go
@@ -1046,8 +1046,6 @@ func kompose(yaml string, namespace string) (string, []runtime.Object, error) {
 	}
 
 	man, err := io.ReadAll(out)
-	log.Println("Kompose object after convert")
-	log.Println(man)
 	if err != nil {
 		return "", nil, err
 	}
